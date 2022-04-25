f766db7dd138a271e186e9eb9fc5cb040157772c
Ayoze <hydekyle@hotmail.com>
2022-04-25 03:37:33 +0100

    working in RPGEvent list

--- Assets/1.Scripts/RPGSystem/RPGUsableItemZone.cs
+++ Assets/1.Scripts/RPGSystem/RPGUsableItemZone.cs
@@ -1,12 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
+
+public enum ActionType { SetSwitch, SetVariable, Talk }
+
+[Serializable]
+public class RPGAction
+{
+    public ActionType actionType;
+    [ShowIf("actionType", ActionType.SetSwitch)]
+    public ConditionTable switches;
+    [ShowIf("actionType", ActionType.Talk)]
+    public string msg;
+}
 
 public class RPGUsableItemZone : MonoBehaviour
 {
     public string requiredItemName;
     public UnityEvent onUsed;
+    public List<RPGAction> actions;
+
 
     public void UsedItem(Item ustedItem)
     {
